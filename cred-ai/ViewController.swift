b7cb16e925cd3bac85c8a8561f5880732ba9c387
leyo babu <leyo@oustlabs.com>
2022-01-23 14:26:47 +0530

    Initial Commit

---
+++ cred-ai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cred-ai
+//
+//  Created by leyo babu on 23/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
